d62d2dde07e68115231b3abe8e0ef382fd384b40
langhuihui <178529795@qq.com>
2023-06-11 16:00:11 +0800

    更新版本号

--- apps/m7s-website/src/components/banner/index.vue
+++ apps/m7s-website/src/components/banner/index.vue
@@ -9,7 +9,7 @@
         <div class="item-2">开源Go语言流媒体服务器开发框架</div>
         <div class="item-3">
           <slot>
-            <span>最新版:v4.6.1</span>
+            <a href="https://mp.weixin.qq.com/s/T12tOpIyVbJGjsDxP_s_Zg">v4.6.3 发布说明</a>
           </slot>
         </div>
         <div class="flex">
